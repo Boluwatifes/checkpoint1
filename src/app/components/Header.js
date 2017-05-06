803c47169d96dd8e57e278c08cc1c4beb17b9b5d
= <greatbolutife@gmail.com>
2017-05-06 02:36:20 +0100

    Login implemented and Default news feed set on hompage

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -19,11 +19,6 @@ class NotLoggedIn extends React.Component {
             <div className="clear" />
           </div>
         </nav>
-        <ul id="slide-out" className="side-nav">
-          <li><a href="#!">First Sidebar Link</a></li>
-          <li><a href="#!">Second Sidebar Link</a></li>
-        </ul>
-        <a href="#" data-activates="slide-out" className="button-collapse show-on-large" id="button-collapse"><i className="material-icons">menu</i></a>
       </header>
     );
   }
@@ -32,30 +27,31 @@ class NotLoggedIn extends React.Component {
 class LoggedIn extends React.Component {
   render() {
     return (
-      <nav className="h-auto p-10">
-        <div className="nav-wrapper">
-          <Link className="brand-logo" to="/">Fast News</Link>
-          <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
-            <li><Link to="/my-news">My News</Link></li>
-            <li><Link to="/portal">News Portal</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-          </ul>
-          <ul className="side-nav" id="mobile-demo">
-            <li><Link to="/my-news">My News</Link></li>
-            <li><Link to="/portal">News Portal</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-          </ul>
-          <div className="clear" />
-        </div>
-      </nav>
+      <header>
+        <nav className="h-auto p-10">
+          <div className="nav-wrapper">
+            <Link className="brand-logo" to="/">Fast News</Link>
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><Link to="/favorites">Favourites</Link></li>
+              <li><img className="avatar-img" src={localStorage.image} alt="User Avatar" /></li>
+              <li><Link to="/logout">Logout</Link></li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><Link to="/favorites">Favorites</Link></li>
+              <li><Link to="/logout">Logout</Link></li>
+            </ul>
+            <div className="clear" />
+          </div>
+        </nav>
+       </header>
     );
   }
 }
 
 export default class Header extends React.Component {
   render() {
-    if (localStorage.getItem('user') !== null) {
+    if (localStorage.getItem('username') !== null) {
       return (<LoggedIn />
       );
     }
--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-// export default function apiAll() {
-//   axios.get('https://newsapi.org/v1/sources?language=en').then((response) => {
-//     return response.data.source;
-//   });
-// }
-
-const apiAll = (callback) => {
+export const apiAll = (callback) => {
   axios.get('https://newsapi.org/v1/sources?language=en')
     .then((response) => {
       callback(response.data.sources);
     });
 };
 
-export default apiAll;
+export const apiOne = (source, callback) => {
+  axios.get(`https://newsapi.org/v1/articles?source=${source}&apiKey=213327409d384371851777e7c7f78dfe`)
+    .then((response) => {
+      callback(response.data.articles);
+    });
+};
+
