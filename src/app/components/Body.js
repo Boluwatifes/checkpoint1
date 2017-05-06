dedb6d91a2e19af5119aedefdce9ce945a3c1191
= <greatbolutife@gmail.com>
2017-05-06 21:56:36 +0100

    Login bug fixed

--- src/app/components/Body.js
+++ src/app/components/Body.js
@@ -9,6 +9,8 @@ export default class Body extends React.Component {
       buttonText: localStorage.getItem('user') ? 'Go To Portal' : 'Get Started',
       url: localStorage.getItem('user') ? '/portal' : '/login',
     };
+    this.responseGoogle = this.responseGoogle.bind(this);
+    this.errorGoogle = this.errorGoogle.bind(this);
   }
 
   responseGoogle(response) {
@@ -17,6 +19,10 @@ export default class Body extends React.Component {
     localStorage.email = profile.getEmail();
     localStorage.image = profile.getImageUrl();
     location.reload();
+  }
+
+  errorGoogle(response) {
+    console.log(response);
   }
 
   render() {
@@ -35,7 +41,7 @@ export default class Body extends React.Component {
                     <GoogleLogin
                       clientId="180417168863-aukt9omvuvpg25ernnc6lgupuv4m3uno.apps.googleusercontent.com"
                       onSuccess={this.responseGoogle}
-                      onFailure={this.responseGoogle}
+                      onFailure={this.errorGoogle}
                       tag="span"
                       disabled="false"
                       style={{ opacity: 1 }}
